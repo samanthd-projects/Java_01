2f96b01047e2f8c2961485fea79fee782085846b
Samanth <samanthd2004@gmail.com>
2025-05-31 10:39:32 +0530

    new code on enum

--- Enum/Enum.java
+++ Enum/Enum.java
@@ -14,7 +14,7 @@ public class Enum {
             System.out.println(status+" "+status.ordinal());
         }
 
-        System.out.println("___________________ ");
+        System.out.println("__________01________ ");
 
         for (Status obj : Status.values()) {
             switch (obj) {
@@ -30,10 +30,26 @@ public class Enum {
                 System.out.println("Bad in switch");
             }
         }
+
+          System.out.println("__________02_________ ");
+
+          F1 team=F1.redbull;
+          System.out.println(team+" == "+team.driver);
+         
     }
 }
 
 
  enum Status{
     Good,Bad,Average
+ }
+
+ enum F1{
+    redbull("max"),rb("lawson"),ferrari("lewis");
+
+    String driver;
+    F1(String name)
+    {
+        this.driver=name;
+    }
  }
